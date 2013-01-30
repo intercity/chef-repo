cb1c23c1d91d300e43247d09e9ac5b1f58773262
Alexander Zaytsev <alexander@say26.com>
2013-01-30 20:49:29 +0600

    Remove rbenv from run list

--- roles/rails.rb
+++ roles/rails.rb
@@ -1,4 +1,4 @@
 name 'rails'
 description 'This role configures a Rails stack using Unicorn'
 default_attributes(:bluepill => {:bin => "/usr/local/bin/bluepill"})
-run_list "recipe[apt]", "recipe[build-essential]", "recipe[percona_mysql]", "recipe[packages]", "recipe[sudo]", "recipe[ruby_build]", "recipe[rbenv::system]", "recipe[bundler]", "recipe[bluepill]", "recipe[nginx]", "recipe[haproxy]", "recipe[backup]", "recipe[rails]", "recipe[git]", "recipe[ssh_deploy_keys]"
+run_list "recipe[apt]", "recipe[build-essential]", "recipe[percona_mysql]", "recipe[packages]", "recipe[sudo]", "recipe[bundler]", "recipe[bluepill]", "recipe[nginx]", "recipe[haproxy]", "recipe[backup]", "recipe[rails]", "recipe[git]", "recipe[ssh_deploy_keys]"
