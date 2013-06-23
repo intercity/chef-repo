dc78e0e59a2b8af093cfe36c7ce3dbc7b5ccf0e4
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-06-23 17:24:00 +0200

    Add configs for knife solo

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,4 +1,5 @@
 cookbook_copyright "Michiel Sikkes"
 cookbook_email "michiel@firmhouse.com"
 cookbook_license "apachev2"
-cookbook_path ["cookbooks"]
+cookbook_path ["./cookbooks"]
+role_path "./roles"
