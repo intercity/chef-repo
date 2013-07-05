7935520892f00016e5c5ecea6a8950e41b338140
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-07-05 13:46:13 +0200

    Only enable ssl when certificate file present

--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -87,7 +87,7 @@ if node[:active_applications]
 
     template "/etc/nginx/sites-available/#{app}.conf" do
       source "app_nginx.conf.erb"
-      variables :name => app, :domain_names => app_info['domain_names']
+      variables :name => app, :domain_names => app_info['domain_names'], :enable_ssl => File.exists?("/u/apps/#{app}/shared/config/certificate.crt")
       notifies :reload, resources(:service => "nginx")
     end
 
