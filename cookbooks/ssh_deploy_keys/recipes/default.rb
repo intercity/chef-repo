f12c6cc6fe11211582a676ffe0b4a5d006ef7074
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-08-18 21:30:33 +0200

    Add deploy keys for all app users

--- cookbooks/ssh_deploy_keys/recipes/default.rb
+++ cookbooks/ssh_deploy_keys/recipes/default.rb
@@ -17,18 +17,20 @@
 # limitations under the License.
 #
 
-if node[:ssh_deploy_keys]
-  directory "/home/deploy/.ssh" do
-    mode 0700
-    owner "deploy"
-    group "deploy"
-  end
+if node[:deploy_users]
+  node[:deploy_users].each do |deploy_user|
+    directory "/home/#{deploy_user}/.ssh" do
+      mode 0700
+      owner deploy_user
+      group deploy_user
+    end
 
-  template "/home/deploy/.ssh/authorized_keys" do
-    source "authorized_keys.erb"
-    mode 0600
-    owner "deploy"
-    group "deploy"
-    variables :keys => node[:ssh_deploy_keys]
+    template "/home/#{deploy_user}/.ssh/authorized_keys" do
+      source "authorized_keys.erb"
+      mode 0600
+      owner deploy_user
+      group deploy_user
+      variables :keys => node[:ssh_deploy_keys]
+    end
   end
 end
