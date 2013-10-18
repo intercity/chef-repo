3e11a342165df9035e341295504810cb859fb2cc
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-10-18 20:13:38 +0200

    Update Rails role to use vendor MySQL server instead of percona

--- roles/rails.rb
+++ roles/rails.rb
@@ -1,3 +1,3 @@
 name 'rails'
 description 'This role configures a Rails stack using Unicorn'
-run_list "recipe[apt]", "recipe[build-essential]", "recipe[percona_mysql]", "recipe[packages]", "recipe[bundler]", "recipe[bluepill]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv::user]", "recipe[backup]", "recipe[rails::databases]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[wkhtmltopdf]", "recipe[postfix]"
+run_list "recipe[apt]", "recipe[build-essential]", "recipe[mysql::server]", "recipe[packages]", "recipe[bundler]", "recipe[bluepill]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv::user]", "recipe[backup]", "recipe[rails::databases]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
