049fd5f3accb97211eeaef12635545833602cd05
Youri van der Lans <youri@itflows.nl>
2013-10-30 20:59:07 +0100

    Added depends for php, nginx and database to cakephp cookbook.

--- cookbooks/cakephp/metadata.rb
+++ cookbooks/cakephp/metadata.rb
@@ -5,3 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures cakephp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends "php"
+depends "nginx"
+depends "database"
