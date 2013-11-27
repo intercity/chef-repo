102d7cc3520f28003fb3ae4998f286095d4a15fc
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-11-27 02:30:57 +0100

    Fix knife.rb to use string as role path

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,5 +2,5 @@ cookbook_copyright "Michiel Sikkes"
 cookbook_email "michiel@firmhouse.com"
 cookbook_license "apachev2"
 cookbook_path ["./cookbooks"]
-role_path ["./roles"]
+role_path "./roles"
 chef_repo_path "."
