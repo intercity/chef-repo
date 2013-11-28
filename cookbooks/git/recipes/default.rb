4c0b3accd6fea8b4fda0e3dc4b6e724aae077a3d
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-11-28 19:56:45 +0100

    Import git version 2.7.0

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: git
 # Recipe:: default
 #
-# Copyright 2012, Michiel Sikkes
+# Copyright 2008-2009, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,39 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-package "git-core"
+case node['platform_family']
+when "debian"
+  if node['platform'] == "ubuntu" && node['platform_version'].to_f < 10.10
+    package "git-core"
+  else
+    package "git"
+  end
+when "rhel","fedora"
+  case node['platform_version'].to_i
+  when 5
+    include_recipe "yum::epel"
+  end
+  package "git"
+when "windows"
+  include_recipe 'git::windows'
+when "mac_os_x"
+  dmg_package "GitOSX-Installer" do
+    app node['git']['osx_dmg']['app_name']
+    package_id node['git']['osx_dmg']['package_id']
+    volumes_dir node['git']['osx_dmg']['volumes_dir']
+    source node['git']['osx_dmg']['url']
+    checksum node['git']['osx_dmg']['checksum']
+    type "pkg"
+    action :install
+  end
+else
+  package "git" do
+    package_name case node['platform']
+                 when 'omnios'
+                   'developer/versioning/git'
+                 when 'smartos'
+                   'scmgit'
+                 end
+  end
+end
