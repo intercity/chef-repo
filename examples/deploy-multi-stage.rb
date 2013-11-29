ac5789354269fc49883809f888e3f325740ceebc
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-11-29 20:28:46 +0100

    Update multistage capistrano sample.

--- examples/deploy-multi-stage.rb
+++ examples/deploy-multi-stage.rb
@@ -1,46 +1,17 @@
 require "bundler/capistrano"
-
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+require "intercity/capistrano"
 
 set :stages, %w(staging production)
 set :default_stage, 'staging'
 
 require 'capistrano/ext/multistage'
 
-set :application, "apphakker"
-set :repository,  "git@github.com:michiels/apphakker.git"
-
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "intercity_sample_app"
+set :repository,  "git@github.com:intercity/intercity_sample_app.git"
 
 role :web, "beta.apphakker.nl"                          # Your HTTP server, Apache/etc
 role :app, "beta.apphakker.nl"                          # This may be the same as your `Web` server
 role :db,  "beta.apphakker.nl", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
-set :user, "deploy"
-set :use_sudo, false
-
 set :deploy_to, defer { "/u/apps/#{application}_#{stage}" }
-
-before "deploy:finalize_update" do
-  run "rm -f #{release_path}/config/database.yml; ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  run "mkdir -p #{release_path}/tmp"
-  run "ln -nfs #{shared_path}/sockets #{release_path}/tmp/sockets"
-end
-
-namespace :deploy do
-  task :start do
-    run "sudo bluepill load /etc/bluepill/#{application}_#{stage}.pill"
-  end
-  task :stop do
-    run "sudo bluepill #{application}_#{stage} stop"
-  end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "sudo bluepill #{application}_#{stage} restart"
-  end
-  task :status do
-    run "sudo bluepill #{application}_#{stage} status"
-  end
-end
--- examples/deploy.rb
+++ examples/deploy.rb
@@ -7,3 +7,4 @@ set :repository,  "git@github.com:intercity/intercity_sample_app.git"
 role :web, "beta.apphakker.nl"                          # Your HTTP server, Apache/etc
 role :app, "beta.apphakker.nl"                          # This may be the same as your `Web` server
 role :db,  "beta.apphakker.nl", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
