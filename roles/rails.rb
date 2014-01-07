1a839ab34e687c68cdd88b2aca43a361dd168d9a
Michiel Sikkes <michiel.sikkes@gmail.com>
2014-01-07 01:36:03 +0100

    Remove bluepill from cookbooks

--- roles/rails.rb
+++ roles/rails.rb
@@ -1,3 +1,3 @@
 name 'rails'
 description 'This role configures a Rails stack using Unicorn'
-run_list "role[base]", "recipe[apt]", "recipe[build-essential]", "recipe[mysql::server]", "recipe[packages]", "recipe[bundler]", "recipe[bluepill]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[rails::databases]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
+run_list "role[base]", "recipe[apt]", "recipe[build-essential]", "recipe[mysql::server]", "recipe[packages]", "recipe[bundler]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[rails::databases]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
