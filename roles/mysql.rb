45add6f05ed8ac843a2f6db9b46299da4a4b5d90
Michiel Sikkes <michiel.sikkes@gmail.com>
2014-01-22 10:16:04 +0100

    Add default attributes for nginx server tokens off and mysql bind to 127.0.0.1 to roles

--- roles/mysql.rb
+++ roles/mysql.rb
@@ -1,3 +1,4 @@
 name 'mysql'
 description 'MySQL server for apps'
 run_list "recipe[build-essential]", "recipe[mysql::server]", "recipe[mysql::client]"
+default_attributes("mysql" => { "bind_address" => "127.0.0.1" })
