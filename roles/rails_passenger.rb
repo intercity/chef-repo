eb635b10e874d145eedfc8512caad2709cab2fa1
Michiel Sikkes <michiel.sikkes@gmail.com>
2014-01-22 10:26:25 +0100

    Put "deploy" as "deploy_user" in default attributes for unicorn and passenger

--- roles/rails_passenger.rb
+++ roles/rails_passenger.rb
@@ -1,4 +1,12 @@
 name 'rails_passenger'
 description 'This role configures a Rails stack using Passenger'
 run_list "role[base]", "recipe[apt]", "recipe[build-essential]", "recipe[packages]", "recipe[mysql::server]", "recipe[rails::passenger]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[rails::databases]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
-default_attributes("nginx" => { "server_tokens" => "off" })
+default_attributes(
+  "nginx" => { "server_tokens" => "off" },
+  "rbenv" => {
+    "group_users" => ['deploy']
+  },
+  "deploy_users" => [
+        "deploy"
+  ]
+)
