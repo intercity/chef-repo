506ed8d851f3c097e28a44db434544de5ecd36d9
Michiel Sikkes <michiel@firmhouse.com>
2014-03-23 20:43:45 +0100

    Allow a different stage for passenger

--- cookbooks/rails/recipes/passenger.rb
+++ cookbooks/rails/recipes/passenger.rb
@@ -122,7 +122,7 @@ if node[:active_applications]
 
     template "/etc/nginx/sites-available/#{app}.conf" do
       source "app_passenger_nginx.conf.erb"
-      variables :name => app, :domain_names => app_info['domain_names'], :enable_ssl => File.exists?("#{applications_root}/#{app}/shared/config/certificate.crt")
+      variables :name => app, :rails_env => rails_env, :domain_names => app_info['domain_names'], :enable_ssl => File.exists?("#{applications_root}/#{app}/shared/config/certificate.crt")
       notifies :reload, resources(:service => "nginx")
     end
 
