841b7a710902f6f161d3548440fb1b9428d89d1f
Oleksandr Iuzikov <oleksandr.iuzikov@gmail.com>
2014-04-17 13:11:31 +0300

    Modified rails/databases recipe to support pg

--- cookbooks/rails/recipes/databases.rb
+++ cookbooks/rails/recipes/databases.rb
@@ -1,30 +1,48 @@
 include_recipe "database"
 
-gem_package "mysql" do
-  gem_binary "/opt/chef/embedded/bin/gem"
-end
-
 if node[:active_applications]
-
   node[:active_applications].each do |app, app_info|
     if app_info['database_info']
-
       database_info = app_info['database_info']
       database_name = app_info['database_info']['database']
 
-      mysql_connection_info = {:host => "localhost", :username => "root", :password => node['mysql']['server_root_password']}
 
-      mysql_database database_name do
-        connection(mysql_connection_info)
-      end
+      if database_info['adapter'] =~ /mysql/
+        chef_gem 'mysql'
 
-      mysql_database_user database_name do
-        connection(mysql_connection_info)
-        username database_info['username']
-        password database_info['password']
-        table "*"
-        host "localhost"
-        action :grant
+        mysql_connection_info = {:host => "localhost", :username => "root", :password => node['mysql']['server_root_password']}
+
+        mysql_database database_name do
+          connection(mysql_connection_info)
+        end
+
+        mysql_database_user database_name do
+          connection(mysql_connection_info)
+          username database_info['username']
+          password database_info['password']
+          table "*"
+          host "localhost"
+          action :grant
+        end
+      elsif database_info['adapter'] =~ /postgres/
+        #chef_gem 'pg'
+
+        #postgresql_database database_name do
+        #  connection(
+        #    :host => 'localhost',
+        #    :port => 5432,
+        #    :username  => "postgres",
+        #    :password  => node['postgresql']['password']['postgres']
+        #  )
+        #  action :create
+        #end
+        execute "create-database" do
+          exists = <<-EOH
+psql -U postgres -c "select * from pg_database WHERE datname='#{database_name}'" | grep -c #{database_name}
+EOH
+          command "createdb -U postgres -O #{database_info['username']} -E utf8 -T template0 #{database_name}"
+          not_if exists
+        end
       end
 
     end
