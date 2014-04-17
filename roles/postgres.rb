7d1072761ce5994fb9a3b5667954204294e0f623
Oleksandr Iuzikov <oleksandr.iuzikov@gmail.com>
2014-04-17 13:11:31 +0300

    Modified rails/databases recipe to support pg

--- roles/postgres.rb
+++ roles/postgres.rb
@@ -1,5 +1,6 @@
-name 'postgresql'
+name 'postgres'
 description 'PostgreSQL Server Support'
 
 run_list 'recipe[build-essential]',
+         'recipe[postgresql::client]',
          'recipe[postgresql::server_debian]'
