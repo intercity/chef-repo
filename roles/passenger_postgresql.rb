c84040192175c79543fa9b36df92899befa005d5
Oleksandr Iuzikov <oleksandr.iuzikov@gmail.com>
2014-04-18 16:48:48 +0300

    Added 'postgresql' community cookbook, removed unneded role, modified postgres role to include postgresql::server recipe in runlist only since it includes client and appropriate platform recipe such as postgresql::server_debian

--- roles/passenger_postgresql.rb
+++
@@ -1,5 +0,0 @@
-name 'passenger_postgresql'
-description 'This role configures a Rails stack using Passenger and PostgreSQL'
-
-run_list 'role[rails]',
-         'recipe[rails::passenger]'
--- roles/postgres.rb
+++ roles/postgres.rb
@@ -1,6 +1,4 @@
-name 'postgres'
-description 'PostgreSQL Server Support'
+name        "postgres"
+description "PostgreSQL Server Support"
 
-run_list 'recipe[build-essential]',
-         'recipe[postgresql::client]',
-         'recipe[postgresql::server_debian]'
+run_list    "recipe[postgresql::server]"
