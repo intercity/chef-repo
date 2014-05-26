f94ada7a133f9498534edf396657cb8747076b70
Michiel Sikkes <michiel.sikkes@gmail.com>
2014-05-26 16:30:22 +0200

    Remove bundler recipe from rails/unicorn stack

--- roles/rails.rb
+++ roles/rails.rb
@@ -1,6 +1,6 @@
 name 'rails'
 description 'This role configures a Rails stack using Unicorn'
-run_list "role[base]", "recipe[packages]", "recipe[bundler]", "recipe[bluepill]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
+run_list "role[base]", "recipe[packages]", "recipe[bluepill]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
 default_attributes(
   "nginx" => { "server_tokens" => "off" },
   "rbenv" => {
@@ -8,5 +8,8 @@ default_attributes(
   },
   "deploy_users" => [
         "deploy"
-  ]
+  ],
+  "bluepill" => {
+    "bin" => "/usr/local/bin/bluepill"
+  }
 )
