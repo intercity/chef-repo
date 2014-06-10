ecde307337d1273d626c50915af2e0e3d5206566
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2014-06-10 11:35:39 +0600

    Moved rails::databases recipe from rails_passanger role to the host settings for consistency (see commit ff8c22f)

--- roles/rails_passenger.rb
+++ roles/rails_passenger.rb
@@ -1,6 +1,6 @@
 name 'rails_passenger'
 description 'This role configures a Rails stack using Passenger'
-run_list "role[base]", "recipe[packages]", "recipe[rails::passenger]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[rails::databases]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
+run_list "role[base]", "recipe[packages]", "recipe[rails::passenger]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
 default_attributes(
   "nginx" => { "server_tokens" => "off", "package_name" => "nginx-extras" },
   "rbenv" => {
