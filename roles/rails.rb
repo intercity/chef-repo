4799feaf4ba014a2cfb9ad1957c9b7c6ba0de40a
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2014-07-30 21:08:58 +0200

    Syntax fix
    Signed-off-by: Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>

--- roles/rails.rb
+++ roles/rails.rb
@@ -3,7 +3,7 @@ description 'This role configures a Rails stack using Unicorn'
 run_list "role[base]", "recipe[packages]", "recipe[bluepill]", "recipe[nginx]", "recipe[rails]", "recipe[ruby_build]", "recipe[rbenv]", "recipe[rails::databases]", "recipe[git]", "recipe[ssh_deploy_keys]", "recipe[postfix]"
 default_attributes(
   "nginx" => { "server_tokens" => "off" },
-  "bluepill": { "bin": "/usr/local/bin/bluepill" },
+  "bluepill" => { "bin" => "/usr/local/bin/bluepill" },
   "rbenv" => {
     "group_users" => ['deploy']
   },
