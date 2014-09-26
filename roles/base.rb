00513ac7e3a2c66d2e980d4fb7f089c13cddc923
Bèr Kessels <ber@berk.es>
2014-09-27 00:12:20 +0200

    Add version 0.1.0 of sysadmins: a way to add sudo users

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,15 @@
 name 'base'
 description 'Base bootstrap for every box'
-run_list "recipe[apt]", "recipe[build-essential]"
+run_list "recipe[sysadmins]", "recipe[sudo]", "recipe[apt]", "recipe[build-essential]"
+default_attributes(
+  "authorization" => {
+    "sudo" => {
+      "passwordless" => true,
+      "include_sudoers_d" => true,
+      "sudoers_default" => [
+        'env_reset',
+        'secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
+      ],
+    }
+  }
+)
