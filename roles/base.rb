5f12ff287e505d4bd738da8c0e3a715276edf6e1
Bèr Kessels <ber@berk.es>
2014-09-28 19:53:40 +0200

    Actually run the sysadmins cookbook on base

--- roles/base.rb
+++ roles/base.rb
@@ -1,6 +1,6 @@
 name 'base'
 description 'Base bootstrap for every box'
-run_list "recipe[sudo]", "recipe[apt]", "recipe[build-essential]"
+run_list "recipe[sysadmins]", "recipe[sudo]", "recipe[apt]", "recipe[build-essential]"
 default_attributes(
   "authorization" => {
     "sudo" => {
