f6e47d8ca32b259e5fa374b37b943267f9750feb
Bèr Kessels <ber@berk.es>
2014-09-29 21:35:58 +0200

    Make sudo work like Ubuntu's defaults.
    * Use a group called "admin"
    * Require a password for sudo.
    
    Requiring a password breaks Vagrant's provision and setup
    and as such, we don't require a password in Vagrant.

--- vendor/cookbooks/sysadmins/metadata.rb
+++ vendor/cookbooks/sysadmins/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "ber@berk.es"
 license "All rights reserved"
 description "Creates sysadmin user accounts"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "0.1.1"
+version "0.1.2"
