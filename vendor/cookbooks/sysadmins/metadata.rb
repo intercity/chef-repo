d065b755bd00e9031c6d1086bfdb8325e890a86c
Michiel Sikkes <michiel@firmhouse.com>
2014-12-29 21:10:59 +0100

    Correctly check if password attribute is entered for a sysadmin

--- vendor/cookbooks/sysadmins/metadata.rb
+++ vendor/cookbooks/sysadmins/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "ber@berk.es"
 license "MIT"
 description "Creates sysadmin user accounts"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "0.1.2"
+version "0.1.3"
