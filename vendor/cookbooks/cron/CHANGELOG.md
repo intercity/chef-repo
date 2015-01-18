b690656e623994d4939f183de71eb18ea42945b2
Bèr Kessels <ber@berk.es>
2015-01-18 16:32:47 +0100

    Add boilerplate for new cron cookbook.

---
+++ vendor/cookbooks/cron/CHANGELOG.md
@@ -0,0 +1,2 @@
+See https://github.com/intercity/chef-repo/blob/master/CHANGELOG.md for full
+changelog
---
+++ vendor/cookbooks/cron/metadata.rb
@@ -0,0 +1,7 @@
+name "cron"
+maintainer "Bèr `berkes` Kessels"
+maintainer_email "ber@berk.es"
+license "MIT"
+description "Schedules tasks for apps"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
