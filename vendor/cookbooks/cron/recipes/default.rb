5534dfdf1efa536a9688dfa53b3cc81905b6cf7a
Bèr Kessels <ber@berk.es>
2015-01-18 18:07:00 +0100

    Add recipe to create per-app crontab entries.
    * Defers attributes as set in node config.
    * Adds crontab to deploy user's crontab unless provided in the node
    * Prefixes command so it runs within app, using rbenv and bundle exec

--- vendor/cookbooks/cron/recipes/default.rb
+++ vendor/cookbooks/cron/recipes/default.rb
@@ -21,3 +21,24 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
+return unless node[:active_applications]
+
+node[:active_applications].map do |app, app_info|
+  app_info["cron"].each do |id, crontab|
+    attributes = crontab.dup
+
+    unscoped_command = attributes.delete("command")
+    rails_env        = app_info['rails_env'] || "production"
+    user             = attributes.delete("user") || app_info['deploy_user'] || "deploy"
+    app_dir          = "#{node[:rails][:applications_root]}/#{app}/current"
+
+    command = "cd #{app_dir} && ( PATH=/opt/rbenv/shims:$PATH RAILS_ENV=#{rails_env} bundle exec #{unscoped_command} )"
+
+    cron "#{app}_#{id}" do
+      command(command)
+      user(user)
+      attributes.each {|attribute, value| send(attribute, value) }
+    end
+  end
+end
