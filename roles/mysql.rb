fdc469b867564f4d25a158681796f254f59ce78e
Oleksandr Iuzikov <oleksandr.iuzikov@gmail.com>
2015-01-30 17:37:39 +0200

    Removed commented code

--- roles/mysql.rb
+++ roles/mysql.rb
@@ -1,4 +1,4 @@
 name 'mysql'
 description 'MySQL server for apps'
-run_list "role[base]" #, "recipe[mysql::server]", "recipe[mysql::client]"
+run_list "role[base]"
 default_attributes("mysql" => { "bind_address" => "127.0.0.1" })
