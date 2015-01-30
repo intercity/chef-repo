e36a1121ce6fa226e04294ffc6590843820b464f
Oleksandr Iuzikov <oleksandr.iuzikov@gmail.com>
2015-01-30 17:35:21 +0200

    Updated mysql cookbook to v 6.0

--- vendor/cookbooks/rails/recipes/databases.rb
+++ vendor/cookbooks/rails/recipes/databases.rb
@@ -1,30 +1,42 @@
-include_recipe "database"
-
 if node[:active_applications]
   node[:active_applications].each do |app, app_info|
     if app_info['database_info']
       database_info = app_info['database_info']
-      database_name = app_info['database_info']['database']
+      
+      database_name     = database_info['database']
       database_username = database_info['username']
       database_password = database_info['password']
 
-      if database_info['adapter'] =~ /mysql/
-        include_recipe 'database::mysql'
 
-        mysql_connection_info = {:host => "localhost", :username => "root", :password => node['mysql']['server_root_password']}
+      if database_info['adapter'] =~ /mysql/
+        mysql_service_name = "default"
+        host = "localhost"
+        root_password = node["mysql"]["server_root_password"]
+
+        mysql_connection_info = {
+          host: host, 
+          username: "root", 
+          password: root_password,
+          socket: "/var/run/mysql-#{mysql_service_name}/mysqld.sock"
+        }
+        
+        mysql_service mysql_service_name do
+          initial_root_password root_password
+          action [:create, :start]
+        end
 
         mysql_database database_name do
-          connection(mysql_connection_info)
+          connection mysql_connection_info
+          action :create
         end
 
         mysql_database_user database_username do
-          connection(mysql_connection_info)
-          username database_username
+          connection mysql_connection_info
           password database_password
-          database_name(database_name)
-          table "*"
+          database_name database_name
           host "localhost"
-          action :grant
+          table "*"
+          action [:create, :grant]
         end
       elsif database_info['adapter'] == 'postgresql'
         execute "create-database-user" do
