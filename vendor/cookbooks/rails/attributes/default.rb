97f9f0c6b31ef2d917be8a0060db5fdbcbfbd269
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2015-04-17 15:00:26 +0200

    Add the unattended-upgrades recipe and set to false by default
    Signed-off-by: Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>

--- vendor/cookbooks/rails/attributes/default.rb
+++ vendor/cookbooks/rails/attributes/default.rb
@@ -1,3 +1,4 @@
+default["apt"]["unattended_upgrades"]["enabled"] = false
 default["rails"]["applications_root"] = "/u/apps"
 default["rbenv"]["binaries_url"] = "http://binaries.intercityup.com/ruby/ubuntu"
 default["rbenv"]["available_binaries"] = %w(1.9.3-p547 2.0.0-p481 2.1.0 2.1.1 2.1.2 2.1.3 2.1.5 2.2.1)
